a99d37709e50b1717a98de9c145be349722744d1
professor Wu <hwu@qcc.cuny.edu>
2024-09-27 13:19:09 -0400

    Lab 8

---
+++ lab8_unittesting/testingcalculation.py
@@ -0,0 +1,45 @@
+"""
+Example 2: Calculation Testing
+Samuel Martinez
+"""
+
+import unittest
+import calculation
+
+class TestCalulation(unittest.TestCase):
+    def test_addthreenumbers(self):
+        self.assertEqual(calculation.addthreenumbers(1,2,3), 6)
+        self.assertEqual(calculation.addthreenumbers(5,3), 8)
+        self.assertEqual(calculation.addthreenumbers(9), 9)
+        self.assertEqual(calculation.addthreenumbers(), 0)
+
+    def test_subtracttwonumbers(self):
+        self.assertEqual(calculation.subtracttwonumbers(2,8), -6)
+        self.assertEqual(calculation.subtracttwonumbers(8,2), 6)
+        self.assertEqual(calculation.subtracttwonumbers(1,2,3), 6)
+
+    def test_dividetwonumbers(self):
+        # test nomral division
+        self.assertEqual(calculation.dividetwonumbers(8,2), 4)
+        self.assertEqual(calculation.dividetwonumbers(-8,2), -4)
+        # float comparison
+        self.assertEqual(calculation.dividetwonumbers(7,2), 3.5)
+
+    # test division by zero
+    def test_dividebyzero(self):
+        self.assertIsNone(calculation.dividetwonumbers(10), 0)
+    
+    # non-numeric testing
+    def test_nonnumericvalues(self):
+        self.assertIsNone(calculation.dividetwonumbers("p", 2))
+        self.assertIsNone(calculation.dividetwonumbers(10, "n"))
+
+    # any another exceptions
+    def test_unexpected_exception(self):
+        # flag raises if 'Excaption' is detected
+        with self.assertRaises(Exception):
+            calculation.dividetwonumbers()
+
+
+if __name__ == "__main__":
+    unittest.main()
